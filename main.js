8b41ba6c1f64af02be81b0118481a0b81900ed15
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2019-01-25 12:43:18 -0500

    Move To Dev Files

--- main.js
+++
@@ -1,7 +0,0 @@
-var name = "Marco";
-var cookie_string = "name=" + name;
-document.cookie = cookie_string;
-document.getElementById('greeting').innerHTML="Hello, "+name;
-
-console.log("cookie_string: " + cookie_string);
-console.log("document.cookie: " + document.cookie);
