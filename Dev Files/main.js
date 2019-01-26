c0d8519a8c11c37fad13385ab79dc5d4fe6b0462
Marco Siciliano <42436088+MasterMs@users.noreply.github.com>
2019-01-26 01:24:57 -0500

    Update main.js

--- Dev Files/main.js
+++ Dev Files/main.js
@@ -1,7 +1,24 @@
-var name = "Marco";
-var cookie_string = "name=" + name;
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+ 	max = Math.floor(max);
+  	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+var cookie_string = "name=";
+id = []
+
+for (i=0; i>10; i++){
+	
+	var num = getRandomInt(0, 9);
+	num.toString();
+	cookie_string = cookie_string +  num;
+	console.log(num)
+
+}
+
 document.cookie = cookie_string;
 document.getElementById('greeting').innerHTML="Hello, "+name;
 
 console.log("cookie_string: " + cookie_string);
 console.log("document.cookie: " + document.cookie);
+
